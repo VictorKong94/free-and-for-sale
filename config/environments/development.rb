558663b126b9019e8152fdce8e6ef45cb433a2a1
Victor Kong <victorkong94@gmail.com>
2016-11-21 01:32:28 -0800

    Enable user authentication

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -27,6 +27,9 @@ Rails.application.configure do
   # number of complex assets.
   config.assets.debug = true
 
+  # ActionMailer Config
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+
   # Adds additional error checking when serving assets at runtime.
   # Checks for improperly declared sprockets dependencies.
   # Raises helpful error messages.
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
 
+  devise_for :users
   root to: 'home#index'
 
   get '/buy_posts/new', to: 'buy_posts#new'
