d7594309c12fe8f23a5a703a69e47dcba402263e
Victor Kong <victorkong94@gmail.com>
2016-11-21 01:56:53 -0800

    Minor debugging

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,8 +1,15 @@
 class HomeController < ApplicationController
 
   def index
+
+    # Organize sale posts
     @all_sale_posts = SalePost.all
+    @all_sale_posts = @all_sale_posts.sort_by { |post| post.updated_at }
+
+    # Organize buy posts
     @all_buy_posts = BuyPost.all
-    @user = User.find(params[:id])
+    @all_buy_posts = @all_buy_posts.sort_by { |post| post.updated_at }
+
   end
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,6 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+  has_many :buy_posts
+  has_many :sale_posts
 end
