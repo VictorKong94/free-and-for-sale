7862fa2bcb172b3394d975af930a5cbdff0febdc
tseclaudia <claudiatse@berkeley.edu>
2016-11-26 00:30:00 -0800

    added button to create posts. doesnt work

--- app/controllers/sale_posts_controller.rb
+++ app/controllers/sale_posts_controller.rb
@@ -9,7 +9,7 @@ class SalePostsController < ApplicationController
   end
 
   def create
-    @sale_post = SalePost.new
+    @sale_post = SalePost.new(new_params)
     @sale_post.name = new_params[:name]
     @sale_post.user_id = current_user.id
     @sale_post.price = new_params[:price]
@@ -37,7 +37,7 @@ class SalePostsController < ApplicationController
   private
 
   def new_params
-    params.require(:item).permit(:name, :price, :photo)
+    params.require(:sale_post).permit(:name, :price, :photo)
   end
 
 end
