29cfaa48033a71c015109b8cc34a24eac8736372
Victor Kong <victorkong94@gmail.com>
2016-11-27 19:11:41 -0800

    Permit parameter :name

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,13 @@
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
-  skip_before_filter :verify_authenticity_token, :only => :create
   protect_from_forgery with: :null_session
+  skip_before_filter :verify_authenticity_token, :only => :create
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+  end
 end
