bb9c2b2f6794390eafd130ee90eda98df96b7b4f
Victor Kong <victorkong94@gmail.com>
2016-11-27 22:52:10 -0800

    Sort posts by last edited

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,11 +4,11 @@ class HomeController < ApplicationController
 
     # Organize sale posts
     @all_sale_posts = SalePost.where(sold: false)
-    @all_sale_posts = @all_sale_posts.sort_by { |post| post.updated_at }
+    @all_sale_posts = @all_sale_posts.sort_by { |f| f.updated_at }.reverse!
 
     # Organize buy posts
     @all_buy_posts = BuyPost.where(filled: false)
-    @all_buy_posts = @all_buy_posts.sort_by { |post| post.updated_at }
+    @all_buy_posts = @all_buy_posts.sort_by { |f| f.updated_at }.reverse!
 
   end
 
