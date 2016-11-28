9ef330c72de69309a3ef2dd4223881feac8cc4d7
Victor Kong <victorkong94@gmail.com>
2016-11-27 18:02:00 -0800

    Alternative

--- app/controllers/buy_posts_controller.rb
+++ app/controllers/buy_posts_controller.rb
@@ -1,5 +1,4 @@
 class BuyPostsController < ApplicationController
-  skip_before_filter :verify_authenticity_token
 
   def index
     @post = BuyPost.all
--- app/controllers/sale_posts_controller.rb
+++ app/controllers/sale_posts_controller.rb
@@ -1,5 +1,4 @@
 class SalePostsController < ApplicationController
-  skip_before_filter :verify_authenticity_token
 
   def index
     @post = SalePost.all
